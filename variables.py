e4360f693f30a5bc4a8e0693d4f131ae03d16e28
Anna <ak102585@student.sgh.waw.pl>
2020-12-07 18:46:40 +0100

    zadanie3

--- variables.py
+++ variables.py
@@ -4,3 +4,15 @@ s2="abcdef'"
 print(s2)
 s3='acd\t\tdb55666d\tij\nklm\\no\płłł'
 print(s3)
+i = 5
+#print('i'+ str(i))
+print('i={0}'.format (i))
+print()
+a='abcdef'
+pos_cd = a.index('cd')
+print(pos_cd)
+
+def index(stack,needle):
+    return stack.index(needle)
+print(index(a,'cd'))
+
---
+++ zad3.py
@@ -0,0 +1,14 @@
+import os
+
+def get_size(start_path = 'C:\dev\Python_Winter_2020_2'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+print(get_size(), 'bytes')
